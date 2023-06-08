572830805bc0bdfacacee3c00e38245c8cb2200f
MUHAMMAD SENDI SIRADJ <muhammad.sendi@ui.ac.id>
2023-06-08 15:53:33 +0700

    add: login func

---
+++ lushlyrics-webapp-django-main/login/decorators.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+from django.conf import settings
+
+def login_exlcluded():
+    def decorator(view_method):
+        def wrapper_func(request, *args, **kwargs):
+            if request.user.is_authenticated :
+                return redirect('default')
+            return view_method(request, *args, **kwargs)
+        return wrapper_func
+    return decorator
+
+
--- lushlyrics-webapp-django-main/login/views.py
+++ lushlyrics-webapp-django-main/login/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .decorators import login_exlcluded
+
 
 # Create your views here.
+@login_exlcluded()
+def login_view(request):
+    if request.method == 'POST' :
+        username_login = request.POST['username']
+        password_login = request.POST['password']
+
+        user = authenticate(request, username=username_login, password_login=password_login)
+        if user is not None:
+            login(request, user)
+            return redirect('default')
+        else :
+            messages.info(request, "Username or password is incorrect!")
